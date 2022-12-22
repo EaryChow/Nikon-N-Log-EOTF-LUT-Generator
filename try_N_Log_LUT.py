caf56c0089abd96d02a1169bd65350e37322f2cc
EaryChow <59176246+EaryChow@users.noreply.github.com>
2022-12-22 11:08:49 -0800

    Add the python script

---
+++ try_N_Log_LUT.py
@@ -0,0 +1,38 @@
+import colour
+import numpy
+import pathlib
+
+output_config_directory = "."
+output_LUTs_directory = "."
+x_input = numpy.linspace(0.0, 1.0, 4096)
+
+
+def n_log_curve(x):
+    curve_lower = ((x/(650/1024))**3) - 0.0075
+    curve_upper = numpy.exp((x-619/1024)/(150/1024))
+
+    curve = numpy.where(x < 0.328, curve_lower, curve_upper)
+
+    return curve
+
+
+y_LUT = n_log_curve(x_input)
+
+aesthetic_LUT_name = "N_Log_Curve_EOTF"
+aesthetic_LUT_safe = aesthetic_LUT_name.replace(" ", "_")
+aesthetic_LUT = colour.LUT1D(
+    table=y_LUT,
+    name="N_Log_Curve_EOTF"
+)
+
+try:
+    output_directory = pathlib.Path(output_config_directory)
+    LUTs_directory = output_directory / output_LUTs_directory
+    LUT_filename = pathlib.Path(
+        LUTs_directory / "{}.spi1d".format(aesthetic_LUT_safe)
+    )
+    LUTs_directory.mkdir(parents=True, exist_ok=True)
+    colour.io.luts.write_LUT(aesthetic_LUT, LUT_filename, method="Sony SPI1D")
+
+except Exception as ex:
+    raise ex
