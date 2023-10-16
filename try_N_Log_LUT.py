e68538124d555ad7692539dbceda377af621e987
EaryChow <59176246+EaryChow@users.noreply.github.com>
2023-10-17 07:05:57 +0800

    Re-arrange and fix breakpoint
    Simplified some lines. The break point was using the float value from the OETF formula, which was wrong, now changed to the 10 bit integer in EOTF formula.

--- try_N_Log_LUT.py
+++ try_N_Log_LUT.py
@@ -2,37 +2,23 @@ import colour
 import numpy
 import pathlib
 
-output_config_directory = "."
-output_LUTs_directory = "."
-x_input = numpy.linspace(0.0, 1.0, 4096)
-
 
 def n_log_curve(x):
-    curve_lower = ((x/(650/1024))**3) - 0.0075
-    curve_upper = numpy.exp((x-619/1024)/(150/1024))
-
-    curve = numpy.where(x < 0.328, curve_lower, curve_upper)
+    curve_lower = ((x / (650 / 1024)) ** 3) - 0.0075
+    curve_upper = numpy.exp((x - 619 / 1024) / (150 / 1024))
+    curve = numpy.where(x < 452/1024, curve_lower, curve_upper)
 
     return curve
 
 
+x_input = numpy.linspace(0.0, 1.0, 4096)
 y_LUT = n_log_curve(x_input)
 
 LUT_name = "N_Log_Curve_EOTF"
 LUT_safe = LUT_name.replace(" ", "_")
 LUT = colour.LUT1D(
     table=y_LUT,
-    name="N_Log_Curve_EOTF"
+    name=LUT_safe
 )
 
-try:
-    output_directory = pathlib.Path(output_config_directory)
-    LUTs_directory = output_directory / output_LUTs_directory
-    LUT_filename = pathlib.Path(
-        LUTs_directory / "{}.spi1d".format(LUT_safe)
-    )
-    LUTs_directory.mkdir(parents=True, exist_ok=True)
-    colour.io.luts.write_LUT(LUT, LUT_filename, method="Sony SPI1D")
-
-except Exception as ex:
-    raise ex
+colour.io.luts.write_LUT(LUT, "{}.spi1d".format(LUT_safe), method="Sony SPI1D")
